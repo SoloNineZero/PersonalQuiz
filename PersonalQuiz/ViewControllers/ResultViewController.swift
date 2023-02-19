1b869b4ff1f35b4b5917c92b4d7b0f784458270b
SoloNineZero <solodyankin.ie@ya.ru>
2023-02-20 00:53:09 +0500

    Initial Commit

---
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz
+//
+//  Created by Alexey Efimov on 19.02.2023.
+//
+
+import UIKit
+
+final class ResultViewController: UIViewController {
+    
+    // 3. Определить наиболее часто встречающийся тип животного
+    // 4. Отобразить результаты в соответствии с этим животным
+    
+    @IBOutlet var animalLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    var answers: [Answer]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+        getAnimal()
+    }
+    
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+}
+
+// MARK: Private Methods
+private extension ResultViewController {
+    func getAnimal() {
+        for answer in answers {
+            print(answer.animal)
+        }
+    }
+}
